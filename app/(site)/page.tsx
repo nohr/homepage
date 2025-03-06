90d15270ec0379c3507dcb9d5db0fab38bf97497
nohr <aiteaigbe@gmail.com>
2025-03-06 04:09:39 -0500

    rm suspense on scan

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,18 +3,15 @@ import Screen from "../_components/screen";
 import { infoQuery } from "@/sanity/lib/queries";
 import CustomPortableText from "./portable-text";
 import Canva from "../(3D)/Canvas";
-import { Suspense } from "react";
 
 export default async function Page() {
   const data = await sanityFetch({ query: infoQuery });
 
   return (
     <Screen className="h-screen min-h-screen">
-      <Suspense>
-        <div className="md:row-start-3 md:row-span-4 md:col-start-1 w-full">
-          <Canva />
-        </div>
-      </Suspense>
+      <div className="md:row-start-3 md:row-span-4 md:col-start-1 w-full">
+        <Canva />
+      </div>
 
       <CustomPortableText
         className="col-start-1 col-span-full row-start-5 row-span-1 md:col-start-9 md:row-start-4 md:col-span-4 md:row-span-3 indent-12"
