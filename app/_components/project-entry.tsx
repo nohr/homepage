13d08dde5a17a23ea8d8cd8749c478834c055bc3
nohr <aiteaigbe@gmail.com>
2025-04-30 11:42:41 -0400

    implement rounded corners

--- app/_components/project-entry.tsx
+++ app/_components/project-entry.tsx
@@ -3,6 +3,7 @@ import { clsx } from "clsx";
 import { PortableText } from "next-sanity";
 import { AiOutlineClose } from "react-icons/ai";
 import PreviewContent from "./preview-content";
+import { motion } from "motion/react";
 
 export default function ProjectEntry({
   project,
@@ -23,17 +24,21 @@ export default function ProjectEntry({
         "flex flex-col gap-4 md:!grid md:grid-cols-6",
       )}
     >
-      <h2
+      <motion.h2
+        whileHover={{ borderColor: "currentColor" }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
         className={clsx(
-          active === project._id && "underline",
+          active === project._id &&
+            " !justify-self-start w-full !border-current",
           active && active !== project._id && "opacity-50",
-          "text-md cursor-pointer w-fit flex flex-row gap-1 items-end h-fit md:col-span-2 md:col-start-5",
+
+          "text-md cursor-pointer w-fit px-1 border border-transparent rounded-full flex flex-row gap-1 justify-self-end items-end h-fit md:col-span-2 md:col-start-5",
         )}
         onClick={() => openPreview(project)}
       >
         {active === project._id && <AiOutlineClose className="!text-sm mb-1" />}
         {project.name}
-      </h2>
+      </motion.h2>
 
       {active === project._id && (
         <>
@@ -42,7 +47,7 @@ export default function ProjectEntry({
             className="md:order-last md:col-start-1 md:row-start-1 md:row-span-2 md:col-span-4 md:h-auto"
           />
 
-          <span className="dark:[&_*]:!text-zinc-400 [&_*]:!text-zinc-600 md:pl-1.5 md:col-span-2 ">
+          <span className="dark:[&_*]:!text-zinc-400 [&_*]:!text-zinc-600  md:col-span-2 ">
             {project.content !== null && project.content !== undefined ? (
               <PortableText value={project.content} />
             ) : null}
